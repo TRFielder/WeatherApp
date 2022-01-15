0c9b632c2e91e70b5b146072d7cb85ad80878327
Tom Fielder <tom.fielder97@gmail.com>
2022-01-15 16:06:54 +0000

    First API calls built to obtain weather

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import * as API from "./weather.js";
+
+const cityname = "Hampton"; //Temporary whilst building API calls
+const theURL = API.getRequestURL("Southampton");
+
+async function getWeatherData(theURL) {
+    const weatherData = await API.requestWeather(theURL);
+    console.log(weatherData);
+    console.log(`The weather in ${cityname} right now is ${weatherData.weather[0].main}`);
+}
+
+getWeatherData(theURL);
--- src/weather.js
+++ src/weather.js
@@ -1,3 +1,19 @@
 //Note: This publicly visible API key is specifically expected
 //See here for the explanation https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/weather-app#api-keys-secrets-and-security
 const key = "875f6ecb3e3a2b867951e413711e4de1" //The openweather API key.
+const cityname = "Hampton"; //Temporary whilst building API calls
+
+function getRequestURL(location) {
+    return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+}
+
+async function requestWeather(url) {
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    return weatherData;
+}
+
+export {
+    getRequestURL,
+    requestWeather,
+};
